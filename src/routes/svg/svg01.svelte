fcdf4d826d92f44e6b21ee539fcca2c2d08c3187
Alersha <73353409+Alersha@users.noreply.github.com>
2022-02-21 17:26:47 +0300

    lead-line as dns

--- src/routes/svg/svg01.svelte
+++ src/routes/svg/svg01.svelte
@@ -4,15 +4,31 @@
 
 <script>
     
-    import LeaderLine from 'leader-line-new';
+    /* import LeaderLine from 'leader-line-new'; */
     /* import { browser } from '$app/env'; */
     import { onMount } from 'svelte';
     import {onDestroy} from 'svelte';
+
+    let line1;
+    /* onMount(async () => {
+		const module = await import('leader-line-new');
+		LeaderLine = module.default;
+		window.ApexCharts = ApexCharts;
+		loaded = true;
+
+        line1 = new LeaderLine(start, end);
+	}); */
+
     
     /* if (browser) {
        let line1 = new LeaderLine(start, end);
     } */
-    let line1;
+    /* $: line1; */
+    /* let start;
+    let end; */
+
+    /* const line1 = new LeaderLine();
+    line1(start, end); */
 
     onMount(/* async */ () => {
        /* const LeaderLine = await import('leader-line-new').default;  */
@@ -30,6 +46,8 @@
 
 
 
+<!-- <div><button bind:this={start}>Element-1</button></div>
+<div>1111111111111111111111111 33333333333 <button bind:this={end}>Element-2</button></div> -->
 <div><button id="start">Element-1</button></div>
 <div>1111111111111111111111111 33333333333 <button id="end">Element-2</button></div>
 
